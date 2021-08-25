f60a7bdefe32f9148746930bdaa36b0d29c70542
Nicole Quijada <quijadabnicolea@gmail.com>
2021-08-25 08:42:19 -0400

    funcion para identificar si es un archivo o directorio y leer los archivos

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const ruta = __dirname;
+
+
+try{
+  const isDirectory = fs.lstatSync('/Users').isDirectory()
+  console.log(isDirectory);
+}catch(error){
+  console.log(error)
+  if(error.code =='ENOENT'){
+
+  }else{
+
+  }
+}
+
+fs.readdir(ruta,(err, archivos) => {
+  archivos.forEach(archivo => {
+    console.log('archivos', archivo);
+  })
+});
