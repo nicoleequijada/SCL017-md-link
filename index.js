05e46dc3e520f1d22f4b71a2de1bb9d422f23678
Nicole Quijada <quijadabnicolea@gmail.com>
2021-08-30 10:28:17 -0400

    funcion para leer archivo

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
+//declarando constantes e importando los modulos de node
 const fs = require('fs');
-const ruta = __dirname;
-
-
-try{
-  const isDirectory = fs.lstatSync('/Users').isDirectory()
-  console.log(isDirectory);
-}catch(error){
-  console.log(error)
-  if(error.code =='ENOENT'){
-
-  }else{
-
-  }
+const pathModule = require('path');
+const EXTENSION_MD = '.md';
+//creo funcion para luego en la funcion principal pueda ser llamada
+//esta funcion verifica si es un directorio
+const isDirectory = (path) => {
+  //usamos el modulo fs e invoco el metodo lstatSync
+  const value = fs.lstatSync(parameterType).isDirectory();
+  return value;
 }
-
-fs.readdir(ruta,(err, archivos) => {
-  archivos.forEach(archivo => {
-    console.log('archivos', archivo);
+//esta funcion se encarga de leer directorios
+const readFolder = (location) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(location, (err, files) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(files)
+      }
+    })
   })
-});
+};
+//esta funcion se encarga de leer un archivo e imprimir su contenido en la terminal
+const printFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        const dataAsString = data.toString();
+        resolve(dataAsString)
+      }
+    })
+  })
+};
+//funcion principal md link verifica si los enlaces estan buenos
+const mdLinks = (path) => {
+  console.log('imprimiendo el path', path)
+if(isDirectory(path)){
+  readFolder(path).then(files => {
+    files.forEach(file => {
+      if (pathModule.extname(file) === EXTENSION_MD) {
+        printFile(file).then(archivo => {
+          console.log(archivo);
+        }).catch(err => console.log(err))
+      }
+    })
+  }).catch(err => console.log(err))
+}
+}
+ 
+// PASO 1 : Lectura de parametro process.argv[2]
+// Declarando variable parameterType y asignamos lo que envia la terminal
+const parameterType = process.argv[2];
+console.log(parameterType)
+//usamos el modulo fs e invoco el metodo lstatSync
+const valor = fs.lstatSync(parameterType).isDirectory();
+console.log('Es un directorio : ' + valor);
+// const filename = 'Users/nicolequijada/Documents/GitHub/SCL017-md-link';
+module.exports.mdLinks = mdLinks(parameterType)
+
+        
