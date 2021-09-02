42734cfaa058bae140850b8e00b03a7bfc1679c5
Nicole Quijada <quijadabnicolea@gmail.com>
2021-09-02 17:03:47 -0400

    resolviendo errores de promesas

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Markdown Links
 
+
 ## Índice
 
 * [1. Preámbulo](#1-preámbulo)
--- index.js
+++ index.js
@@ -1,91 +1,102 @@
 //declarando constantes e importando los modulos de node
-const fs = require('fs');
-const pathModule = require('path');
+import fs from 'fs';
+import  pathModule from 'path';
 const extMd = '.md';
 const regEx = /\]\((http[^)]+)\)/g;
-
-// se creo funcion para luego en la funcion principal pueda ser llamada
-//esta funcion verifica si es un directorio
-const isDirectory = (path) => {
-  //usamos el modulo fs e invoco el metodo lstatSync
-  const value = fs.lstatSync(parameterType).isDirectory();
-  return value;
+import fetch from 'node-fetch';
+let linksBueno = [];
+let linksMalo = [];
+const headers = {
+  "Content-Type": "application/json"
 }
-//esta funcion se encarga de leer directorios o carpetas
-const readFolder = (location) => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(location, (err, files) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(files)
-      }
-    })
-  })
-};
-//esta funcion se encarga de leer un archivo e imprimir su contenido en la terminal
-const printFile = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        let arrayLinks = [];
-        const dataAString = data.toString();
-        const allLink = dataAString.match(regEx);
-        allLink.forEach(e => {
-          console.log('match dentro del forEach', e)
-          arrayLinks.push(e.replace(/[\[\(\)\]]/g, ''))
-        })
-        resolve(arrayLinks)
-      }
-    })
-  })
-};
 
-//funcion para leer archivos MD
-const mdLinks = (path) => {
-  console.log('imprimiendo el path', path)
-if(isDirectory(path)){
-  readFolder(path).then(files => {
-    files.forEach(file => {
-      if (pathModule.extname(file) === extMd) {
-        printFile(file).then(links => {
-          let linksBueno = [];
-          let linksMalo = [];
-          links.forEach(link =>{
-            if(isValidUrl(link)){
-              linksBueno.push(link)
-            }else{
-              linksMalo.push(link)
-            }
-            // console.log({linksBueno})
-            // console.log({linksMalo})
-          })
+const validateUrls = (urls) => {
+ return Promise.all(urls.map(url =>
+    fetch(url).then(resp => resp)
+    )).then(texts => {
+      return texts
+    })
+    .catch(err => console.log("no funciona"))
     
-          // console.log(links);
-        }).catch(err => console.log(err))
-      }
+    // fetch(url , {headers:headers, mode: 'no-cors'})
+    // .then(response => {
+    //   if(response.status === 200){
+    //     // console.log("links buenos")
+    //     linksBueno.push(url)
+    //   }
+    // })
+    // .catch(err => linksMalo.push(url)) 
+    
+  };
+  
+  //esta funcion se encarga de leer directorios o carpetas
+  const readFolder = (location) => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(location, (err, files) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(files)
+        }
+      })
     })
-  }).catch(err => console.log(err))
-}
-}
- 
-// PASO 1 : Lectura de parametro process.argv[2]
-// Declarando variable parameterType y asignamos lo que envia la terminal
-const parameterType = process.argv[2];
-// console.log(parameterType)
-//usamos el modulo fs e invoco el metodo lstatSync
-const valor = fs.lstatSync(parameterType).isDirectory();
-console.log('Es un directorio : ' + valor);
-module.exports.mdLinks = mdLinks(parameterType)
-
-const isValidUrl = (url) => {
-  try {
-    new URL(url);
-  } catch (e) {
-    console.error(e);
-    return false;
+  };
+  //esta funcion se encarga de leer un archivo e imprimir su contenido en la terminal
+  const printFile = (filename) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          let arrayLinks = [];
+          const dataAString = data.toString();
+          const allLink = dataAString.match(regEx);
+          allLink.forEach(element => {
+            // console.log('match dentro del forEach', element)
+            arrayLinks.push(element.replace(/[\[\(\)\]]/g, ''))
+          })
+          resolve(arrayLinks)
+        }
+      })
+    })
+  };
+  
+  //funcion para leer archivos MD
+  const mdLinks = (path) => {
+    // console.log('imprimiendo el path', path)
+    if(fs.lstatSync(path).isDirectory()){
+      readFolder(path).then(files => {
+        files.forEach(file => {
+          if (pathModule.extname(file) === extMd) {
+            printFile(file).then(links => {
+              
+             const urlsArray = validateUrls(links).then(newLinks => newLinks)
+             .then(a => a)
+             console.log(urlsArray)
+             urlsArray.map(currentResponse => {
+               if(currentResponse.status === 200){
+                 linksBueno.push(currentResponse.url)
+               }else{
+                 linksMalo.push(currentResponse.url)
+               }
+             })
+              
+              // console.log(links);
+            }).catch(err => console.log(err))
+          }
+        })
+      }).catch(err => console.log(err))
+    }
+    console.log({linksBueno})
+    console.log({linksMalo})
   }
-  return true;
-};
+  
+  // PASO 1 : Lectura de parametro process.argv[2]
+  // Declarando variable parameterType y asignamos lo que envia la terminal
+  const parameterType = process.argv[2];
+  // console.log(parameterType)
+  //usamos el modulo fs e invoco el metodo lstatSync
+  mdLinks(parameterType)
+  
+  
+  
